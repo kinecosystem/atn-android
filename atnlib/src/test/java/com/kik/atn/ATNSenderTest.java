c9e4af327b3dd5188a4e1c1ac5f0ae9479efc830
Yossi Rizgan <yossi.rizgan@kik.com>
2018-02-26 09:12:23 +0200

    add testing libs

---
+++ atnlib/src/test/java/com/kik/atn/ATNSenderTest.java
@@ -0,0 +1,22 @@
+package com.kik.atn;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ATNSenderTest {
+
+
+    private ATNSender sender;
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void sendATN() throws Exception {
+    }
+
+}
