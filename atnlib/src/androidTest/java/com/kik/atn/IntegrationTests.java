303e9c8cff445bb5230c03787c62c32f69bd608a
Yossi Rizgan <yossi.rizgan@kik.com>
2018-03-04 12:50:37 +0200

    additional tests

--- atnlib/src/androidTest/java/com/kik/atn/IntegrationTests.java
+++ atnlib/src/androidTest/java/com/kik/atn/IntegrationTests.java
@@ -7,41 +7,52 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
 
+import kin.core.Balance;
 import kin.core.KinAccount;
+import kin.core.exception.AccountNotFoundException;
+import kin.core.exception.OperationFailedException;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 import static java.lang.Thread.sleep;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class IntegrationTests {
 
 
+    private static final String PUBLIC_ADDRESS = "GDYF6ZDSSLM32OKGOL6ZKA4JYSBFSHLSARUUPE4YDYNOHJ5WXSLMBDUV";
     @Mock
     private KinAccount mockKinAccount;
     private MockWebServer mockWebServer;
     private ATN atn;
+    private String mockServerUrl;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
 
-        when(mockKinAccount.getPublicAddress()).thenReturn("GDYF6ZDSSLM32OKGOL6ZKA4JYSBFSHLSARUUPE4YDYNOHJ5WXSLMBDUV");
+        when(mockKinAccount.getPublicAddress()).thenReturn(PUBLIC_ADDRESS);
 
         Context appContext = InstrumentationRegistry.getTargetContext();
         mockWebServer = new MockWebServer();
-        String url = mockWebServer.url("").toString();
-        TestModulesProvider modulesProvider = new TestModulesProvider(appContext, url, mockKinAccount);
+        mockServerUrl = mockWebServer.url("").toString();
+        TestModulesProvider modulesProvider = new TestModulesProvider(appContext, mockServerUrl, mockKinAccount);
 
         atn = new ATN(modulesProvider);
     }
@@ -55,6 +66,8 @@ public class IntegrationTests {
                         "     \"target_wallet_address\": \"GBNU4TLYIQOQBM3PT32Z3CCYSMI6CDK7FXQR6R5DYB52GUPXES2S6XTU\"\n" +
                         "}")
                 .setResponseCode(200));
+        //not onboarded yet
+        doThrow(new AccountNotFoundException("")).when(mockKinAccount).getBalanceSync();
         //create account
         mockWebServer.enqueue(new MockResponse().setResponseCode(200));
         //fund with ATN
@@ -62,12 +75,136 @@ public class IntegrationTests {
 
         atn.onMessageSent(InstrumentationRegistry.getTargetContext());
         atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        sleep(2000);
+
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+
+        verify(mockKinAccount, timeout(1000).times(1))
+                .sendTransactionSync(eq("GBNU4TLYIQOQBM3PT32Z3CCYSMI6CDK7FXQR6R5DYB52GUPXES2S6XTU")
+                        , anyString(), eq(new BigDecimal(1.0)));
+    }
+
+    @Test
+    public void init_AlreadyOnboarded() throws Exception {
+        mockAlreadyOnBoarded();
+
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
         sleep(3000);
 
         atn.onMessageSent(InstrumentationRegistry.getTargetContext());
 
+        verify(mockKinAccount, timeout(1000).times(1)).getPublicAddress();
+        verify(mockKinAccount, timeout(1000).times(1)).getBalanceSync();
         verify(mockKinAccount, timeout(1000).times(1)).sendTransactionSync(anyString(), anyString(), (BigDecimal) any());
+        verifyNoMoreInteractions(mockKinAccount);
     }
 
+    private void mockAlreadyOnBoarded() throws OperationFailedException {
+        //configuration
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("{\n" +
+                        "     \"enabled\" : true,\n" +
+                        "     \"target_wallet_address\": \"GBNU4TLYIQOQBM3PT32Z3CCYSMI6CDK7FXQR6R5DYB52GUPXES2S6XTU\"\n" +
+                        "}")
+                .setResponseCode(200));
+        when(mockKinAccount.getBalanceSync()).thenReturn(new Balance() {
+            @Override
+            public BigDecimal value() {
+                return new BigDecimal(10);
+            }
+
+            @Override
+            public String value(int precision) {
+                return "10.0";
+            }
+        });
+    }
+
+    @Test
+    public void init_NotEnabled() throws Exception {
+        //configuration
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("{\n" +
+                        "     \"enabled\" : false,\n" +
+                        "     \"target_wallet_address\": \"GBNU4TLYIQOQBM3PT32Z3CCYSMI6CDK7FXQR6R5DYB52GUPXES2S6XTU\"\n" +
+                        "}")
+                .setResponseCode(200));
+
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        sleep(1000);
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        sleep(1000);
+        verify(mockKinAccount, timeout(1000).only()).getPublicAddress();
+    }
+
+    @Test
+    public void onMessageReceived() throws Exception {
+        ATNServer mockATNServer = mock(ATNServer.class);
+        TestModulesProvider modulesProvider = new TestModulesProvider(InstrumentationRegistry.getTargetContext(), mockServerUrl, mockKinAccount,
+                mockATNServer);
+
+        atn = new ATN(modulesProvider);
+
+        mockAlreadyOnBoarded();
+
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+        sleep(1000);
+
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+
+        verify(mockATNServer, timeout(1000).times(1)).receiveATN(PUBLIC_ADDRESS);
+        verifyNoMoreInteractions(mockATNServer);
+    }
+
+    @Test
+    public void sentAndReceived() throws Exception {
+        ATNServer mockATNServer = mock(ATNServer.class);
+        TestModulesProvider modulesProvider = new TestModulesProvider(InstrumentationRegistry.getTargetContext(), mockServerUrl, mockKinAccount,
+                mockATNServer);
+
+        atn = new ATN(modulesProvider);
+
+        mockAlreadyOnBoarded();
+
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+        sleep(1000);
+
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+
+        InOrder inOrder = inOrder(mockKinAccount, mockATNServer);
+        inOrder.verify(mockATNServer, timeout(1000).times(1))
+                .receiveATN(PUBLIC_ADDRESS);
+        inOrder.verify(mockKinAccount, timeout(1000).times(1))
+                .sendTransactionSync(anyString(), anyString(), (BigDecimal) any());
+    }
+
+    @Test
+    public void receivedAndSent() throws Exception {
+        ATNServer mockATNServer = mock(ATNServer.class);
+        TestModulesProvider modulesProvider = new TestModulesProvider(InstrumentationRegistry.getTargetContext(), mockServerUrl, mockKinAccount,
+                mockATNServer);
+
+        atn = new ATN(modulesProvider);
+
+        mockAlreadyOnBoarded();
+
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        sleep(1000);
+
+        atn.onMessageSent(InstrumentationRegistry.getTargetContext());
+        atn.onMessageReceived(InstrumentationRegistry.getTargetContext());
+
+        InOrder inOrder = inOrder(mockKinAccount, mockATNServer);
+        inOrder.verify(mockKinAccount, timeout(1000).times(1))
+                .sendTransactionSync(anyString(), anyString(), (BigDecimal) any());
+        inOrder.verify(mockATNServer, timeout(1000).times(1))
+                .receiveATN(PUBLIC_ADDRESS);
+    }
 
 }
--- atnlib/src/androidTest/java/com/kik/atn/TestModulesProvider.java
+++ atnlib/src/androidTest/java/com/kik/atn/TestModulesProvider.java
@@ -16,15 +16,22 @@ class TestModulesProvider extends ModulesProvider {
         this.account = account;
     }
 
+    TestModulesProvider(Context context, String serverUrl, KinAccount account, ATNServer server) {
+        this(context, serverUrl, account);
+        this.atnServer = server;
+    }
+
     @Override
     protected void inject(Context context) {
         this.androidLogger = new AndroidLogger();
-        this.atnServer = new ATNServer(new ATNServerURLProvider() {
-            @Override
-            String getUrl() {
-                return serverUrl;
-            }
-        });
+        if (this.atnServer == null) {
+            this.atnServer = new ATNServer(new ATNServerURLProvider() {
+                @Override
+                String getUrl() {
+                    return serverUrl;
+                }
+            });
+        }
         this.eventLogger = new EventLogger(atnServer, androidLogger, true);
         this.configurationProvider = new ConfigurationProvider(atnServer, eventLogger);
         this.onboarding = new ATNAccountOnBoarding(eventLogger, atnServer);
